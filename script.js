d26c742c01ee844e423d19780f8a2e13e50b38b1
Uwakmfon James <contactuwak@gmail.com>
2026-06-12 22:07:28 +0100

    initial codes

---
+++ script.js
@@ -0,0 +1,37 @@
+
+gsap.from(".logo",{
+  y: -100,
+  duration: 2
+})
+
+gsap.from(".menu",{
+  y: -100,
+  duration: 2
+  
+})
+
+gsap.from(".login-btn",{
+  y: -100,
+  duration: 2
+})
+
+gsap.from("h1", {
+  y:"-200%",
+  duration: 1,
+  delay: 0
+
+})
+
+gsap.from("p", {
+  y:"120%",
+  duration: 1,
+  delay: 0
+
+})
+
+gsap.from(".background-img", {
+  x:"-100%",
+  duration: 1,
+  delay: 0
+
+})
